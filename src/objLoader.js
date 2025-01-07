3f7868b179bdec5def1259e8c31095c53e0d9c2d
someoneplanet <moaipp9@gmail.com>
2025-01-07 15:22:27 -0300

    Update objLoader.js

--- src/objLoader.js
+++ src/objLoader.js
@@ -17,6 +17,13 @@ export function loadOBJWithMTL(objPath, mtlPath, scene) {
     });
 }
 
+export function loadOBJFromText(objText, scene) {
+    const objLoader = new THREE.OBJLoader();
+    const object = objLoader.parse(objText);
+    clearScene(scene);
+    scene.add(object);
+}
+
 function clearScene(scene) {
     while (scene.children.length > 2) {
         scene.remove(scene.children[scene.children.length - 1]);
